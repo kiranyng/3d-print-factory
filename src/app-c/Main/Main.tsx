0ad48e2c206f2ef5f9ba796db10e98dc378e74f8
KiranYNG <kiran.yng@gmail.com>
2021-10-11 22:29:05 +0530

    minor refactoring

---
+++ src/app-c/Main/Main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Print Factory
+      </header>
+      <div className="App-left">
+        left
+      </div>
+      <div className="App-mid">
+        mid
+      </div>
+      <div className="App-right">
+        right
+      </div>
+      <div className="App-footer">
+        footer
+      </div>
+    </div>
+  );
+}
+
+export default Main;
